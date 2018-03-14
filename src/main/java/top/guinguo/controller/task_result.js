578f820ba169294ae6072821f077940b2e5902f8
guiguin_guo <guin_guo@yahoo.com>
2018-03-15 00:07:47 +0800

    + restrict task result format

---
+++ src/main/java/top/guinguo/controller/task_result.js
@@ -0,0 +1,477 @@
+/**
+ * Created by guin_guo on 2018/3/11.
+ */
+task_result = {
+    "userDimensionChart": {
+        "preferWords": {
+            "top5": {
+                "latest": [{
+                    "date": "2018-03-02",
+                    "postText": "转发微博",
+                    "postUrl": "http://weibo.com/1898267100/G5xA8vquN",
+                    "postCutText": "转发微博"
+                }, {
+                    "date": "2018-02-24",
+                    "postText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​",
+                    "postUrl": "http://weibo.com/1898267100/G4CPlvEm8",
+                    "postCutText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​..."
+                }, {
+                    "date": "2018-02-24",
+                    "postText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizqe ​",
+                    "postUrl": "http://weibo.com/1898267100/G4Ab1sCCA",
+                    "postCutText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizq..."
+                }, {
+                    "date": "2018-02-18",
+                    "postText": "//@谁看见了我的翰林:转发微博",
+                    "postUrl": "http://weibo.com/1898267100/G3Ir67knv",
+                    "postCutText": "//@谁看见了我的翰林:转发微博"
+                }, {
+                    "date": "2018-02-18",
+                    "postText": "美得不行",
+                    "postUrl": "http://weibo.com/1898267100/G3IpL6EhF",
+                    "postCutText": "美得不行"
+                }],
+                "mostComments": [{
+                    "commentsSize": 13,
+                    "postText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n@Una娜娜儿 @王小夏同学 ​",
+                    "postUrl": "http://weibo.com/1898267100/FCfrEck6u",
+                    "postCutText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n..."
+                }, {
+                    "commentsSize": 2,
+                    "postText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：Una娜娜儿\nhttp://t.cn/ROzhwMj ​",
+                    "postUrl": "http://weibo.com/1898267100/FzNGAi7U1",
+                    "postCutText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：..."
+                }, {
+                    "commentsSize": 2,
+                    "postText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​",
+                    "postUrl": "http://weibo.com/1898267100/G1Up3yW4U",
+                    "postCutText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​"
+                }, {
+                    "commentsSize": 1,
+                    "postText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​",
+                    "postUrl": "http://weibo.com/1898267100/G4CPlvEm8",
+                    "postCutText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​..."
+                }, {
+                    "commentsSize": 1,
+                    "postText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizqe ​",
+                    "postUrl": "http://weibo.com/1898267100/G4Ab1sCCA",
+                    "postCutText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizq..."
+                }],
+                "mostForwards": [{
+                    "forwardSize": 2,
+                    "postText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n@Una娜娜儿 @王小夏同学 ​",
+                    "postUrl": "http://weibo.com/1898267100/FCfrEck6u",
+                    "postCutText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n..."
+                }, {
+                    "forwardSize": 2,
+                    "postText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​",
+                    "postUrl": "http://weibo.com/1898267100/G1Up3yW4U",
+                    "postCutText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​"
+                }, {
+                    "forwardSize": 0,
+                    "postText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：Una娜娜儿\nhttp://t.cn/ROzhwMj ​",
+                    "postUrl": "http://weibo.com/1898267100/FzNGAi7U1",
+                    "postCutText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：..."
+                }, {
+                    "forwardSize": 0,
+                    "postText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​",
+                    "postUrl": "http://weibo.com/1898267100/G4CPlvEm8",
+                    "postCutText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​..."
+                }, {
+                    "forwardSize": 0,
+                    "postText": "//@谁看见了我的翰林:转发微博",
+                    "postUrl": "http://weibo.com/1898267100/G3Ir67knv",
+                    "postCutText": "//@谁看见了我的翰林:转发微博"
+                }],
+                "mostLikes": [{
+                    "likeSize": 20,
+                    "postText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n@Una娜娜儿 @王小夏同学 ​",
+                    "postUrl": "http://weibo.com/1898267100/FCfrEck6u",
+                    "postCutText": "誰が見ても\nわれをなつかしくなるごとき長き手紙を書きたき夕\n..."
+                }, {
+                    "likeSize": 8,
+                    "postText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​",
+                    "postUrl": "http://weibo.com/1898267100/G1Up3yW4U",
+                    "postCutText": "万物皆有裂痕，那是光照进来的地方。\n出镜：@阿欠又 ​"
+                }, {
+                    "likeSize": 6,
+                    "postText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：Una娜娜儿\nhttp://t.cn/ROzhwMj ​",
+                    "postUrl": "http://weibo.com/1898267100/FzNGAi7U1",
+                    "postCutText": "于是，素履之往，往事之微，只道一句：你好，别来无恙。\n出镜：..."
+                }, {
+                    "likeSize": 2,
+                    "postText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizqe ​",
+                    "postUrl": "http://weibo.com/1898267100/G4Ab1sCCA",
+                    "postCutText": "坚持熬夜成功变成双眼皮 http://t.cn/R2Wizq..."
+                }, {
+                    "likeSize": 2,
+                    "postText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​",
+                    "postUrl": "http://weibo.com/1898267100/G4CPlvEm8",
+                    "postCutText": "可能系石澳大家姐 http://t.cn/Rx1o9mp ​..."
+                }]
+            },
+            "wordCloud": {
+                "weiBoWordFor90": [{
+                    "text": "转发",
+                    "size": 2
+                }, {
+                    "text": "娜娜",
+                    "size": 2
+                }, {
+                    "text": "una",
+                    "size": 2
+                }, {
+                    "text": "光照",
+                    "size": 1
+                }, {
+                    "text": "不行",
+                    "size": 1
+                }, {
+                    "text": "大家",
+                    "size": 1
+                }, {
+                    "text": "r2wizqe",
+                    "size": 1
+                }, {
+                    "text": "成功",
+                    "size": 1
+                }, {
+                    "text": "裂痕",
+                    "size": 1
+                }, {
+                    "text": "坚持",
+                    "size": 1
+                }, {
+                    "text": "见了",
+                    "size": 1
+                }, {
+                    "text": "一句",
+                    "size": 1
+                }, {
+                    "text": "同学",
+                    "size": 1
+                }, {
+                    "text": "万物",
+                    "size": 1
+                }, {
+                    "text": "那是",
+                    "size": 1
+                }, {
+                    "text": "熬夜",
+                    "size": 1
+                }, {
+                    "text": "往事",
+                    "size": 1
+                }, {
+                    "text": "地方",
+                    "size": 1
+                }, {
+                    "text": "rx1o9mp",
+                    "size": 1
+                }, {
+                    "text": "谁看",
+                    "size": 1
+                }, {
+                    "text": "皆有",
+                    "size": 1
+                }, {
+                    "text": "可能",
+                    "size": 1
+                }, {
+                    "text": "双眼皮",
+                    "size": 1
+                }, {
+                    "text": "别来无恙",
+                    "size": 1
+                }, {
+                    "text": "rozhwmj",
+                    "size": 1
+                }, {
+                    "text": "翰林",
+                    "size": 1
+                }, {
+                    "text": "美得",
+                    "size": 1
+                }, {
+                    "text": "于是",
+                    "size": 1
+                }, {
+                    "text": "变成",
+                    "size": 1
+                }],
+                "weiBoWordFor30": [{
+                    "text": "转发",
+                    "size": 2
+                }, {
+                    "text": "变成",
+                    "size": 1
+                }, {
+                    "text": "可能",
+                    "size": 1
+                }, {
+                    "text": "见了",
+                    "size": 1
+                }, {
+                    "text": "成功",
+                    "size": 1
+                }, {
+                    "text": "r2wizqe",
+                    "size": 1
+                }, {
+                    "text": "双眼皮",
+                    "size": 1
+                }, {
+                    "text": "坚持",
+                    "size": 1
+                }, {
+                    "text": "rx1o9mp",
+                    "size": 1
+                }, {
+                    "text": "翰林",
+                    "size": 1
+                }, {
+                    "text": "大家",
+                    "size": 1
+                }, {
+                    "text": "不行",
+                    "size": 1
+                }, {
+                    "text": "熬夜",
+                    "size": 1
+                }, {
+                    "text": "美得",
+                    "size": 1
+                }, {
+                    "text": "谁看",
+                    "size": 1
+                }],
+                "mentionFor90": [],
+                "mentionFor30": []
+            }
+        },
+        "myInterests": {
+            "myInterestsItemses": [{
+                "text": "随手拍",
+                "score": 2109.9
+            }, {
+                "text": "cosplay",
+                "score": 1644.4
+            }, {
+                "text": "人物摄影",
+                "score": 1454.2
+            }, {
+                "text": "搞笑段子",
+                "score": 1420.4
+            }, {
+                "text": "漫画",
+                "score": 1082.3
+            }, {
+                "text": "摇滚音乐",
+                "score": 672.6
+            }, {
+                "text": "卡通萌图",
+                "score": 656.3
+            }, {
+                "text": "风景摄影",
+                "score": 654.1
+            }, {
+                "text": "爱音乐",
+                "score": 629.1
+            }, {
+                "text": "赛车",
+                "score": 610.7
+            }]
+        },
+        "myFans": {
+            "sexRatio": {
+                "man": {
+                    "number": 45,
+                    "ratio": "56.3%"
+                },
+                "woman": {
+                    "number": 35,
+                    "ratio": "43.8%"
+                },
+                "unknown": {
+                    "number": 0,
+                    "ratio": "0.0%"
+                }
+            },
+            "arealDistribution": {
+                "city": {
+                    "arealDistributionItems": [{
+                        "name": "云浮市",
+                        "value": 21
+                    }, {
+                        "name": "广州市",
+                        "value": 14
+                    }, {
+                        "name": "中山市",
+                        "value": 9
+                    }, {
+                        "name": "佛山市",
+                        "value": 8
+                    }, {
+                        "name": "肇庆市",
+                        "value": 3
+                    }, {
+                        "name": "东莞市",
+                        "value": 3
+                    }, {
+                        "name": "江门市",
+                        "value": 2
+                    }, {
+                        "name": "惠州市",
+                        "value": 2
+                    }, {
+                        "name": "深圳市",
+                        "value": 1
+                    }, {
+                        "name": "珠海市",
+                        "value": 1
+                    }, {
+                        "name": "天津",
+                        "value": 1
+                    }, {
+                        "name": "南宁市",
+                        "value": 1
+                    }, {
+                        "name": "黄浦区",
+                        "value": 1
+                    }],
+                    "topXArealDistributionItems": [{
+                        "name": "云浮市",
+                        "value": 21
+                    }, {
+                        "name": "广州市",
+                        "value": 14
+                    }, {
+                        "name": "中山市",
+                        "value": 9
+                    }, {
+                        "name": "佛山市",
+                        "value": 8
+                    }, {
+                        "name": "肇庆市",
+                        "value": 3
+                    }]
+                }
+            },
+            "hierarchicalDistribution": {
+                "hierarchicalDistributionItems": [{
+                    "count": 40,
+                    "level": "0-10级"
+                }, {
+                    "count": 15,
+                    "level": "11-20级"
+                }, {
+                    "count": 16,
+                    "level": "21-30级"
+                }, {
+                    "count": 8,
+                    "level": "31-40级"
+                }, {
+                    "count": 1,
+                    "level": "41-48级"
+                }]
+            },
+            "twitterAge": {
+                "50后": 0,
+                "60后": 0,
+                "70后": 0,
+                "80后": 1,
+                "90后": 41,
+                "95后": 3
+            },
+            "memberType": {
+                "普通": 50,
+                "达人": 28,
+                "个人认证": 2,
+                "企业认证": 0,
+                "微博女郎": 0,
+                "其它": 0
+            },
+            "fansRange": {
+                "0-1千": 72,
+                "1千-2千": 4,
+                "2千-5千": 4,
+                "5千-1万": 0,
+                "1万-10万": 0,
+                "10万-50万": 0,
+                "50万-1百万": 0,
+                "1百万-5百万": 0,
+                "5百万-1千万": 0,
+                "1千万-1亿": 0
+            },
+            "companyTag": [{
+                "text": "保利",
+                "size": 1
+            }, {
+                "text": "学校",
+                "size": 1
+            }, {
+                "text": "国贸",
+                "size": 1
+            }, {
+                "text": "药学院",
+                "size": 1
+            }, {
+                "text": "动漫",
+                "size": 1
+            }, {
+                "text": "德国",
+                "size": 1
+            }, {
+                "text": "副社长",
+                "size": 1
+            }, {
+                "text": "开火",
+                "size": 1
+            }],
+            "schoolRank": {
+                "广东药学院": 24,
+                "新兴一中": 9,
+                "石门中学": 3,
+                "南方医科大学": 2,
+                "四会中学": 2,
+                "广州城建职业学院": 2,
+                "华南农业大学": 2,
+                "肇庆学院": 1,
+                "广东商学院": 1,
+                "重庆师范大学": 1
+            },
+            "fansNum": 80
+        }
+    },
+    "userDimensionInfo": {
+        "preferWordsInfo": {
+            "taskHasDone": true,
+            "item": ["转发", "娜娜", "una"]
+        },
+        "myInterestsInfo": {
+            "taskHasDone": true,
+            "text": "随手拍 cosplay 人物摄影 搞笑段子 漫画 摇滚音乐 卡通萌图 风景摄影 爱音乐 赛车 流行搭配 电影电视 极限运动 汽车百科 重金属音乐 美食 改装车 IT数码 手机 欧美影视 婚纱礼服 汽车品牌 日本动漫 VI设计 篮球 时尚潮流 专业摄影 耽美 读书阅读 社会热点 成人情趣 天文地理 壁纸美图 外语学习 米粉 自然科学 电子休闲游戏 收藏 日韩娱乐 国产手机 生活百科 日韩音乐 宠物 旅游旅行 心灵鸡汤 慈善公益 运动品牌 时政新闻 星座星运 海外生活 骑行 网购 美容美妆 欧美音乐 园艺 广告设计 果粉 家居设计 微电影 华语音乐 日本影视 古典音乐 外国手机 计算机 历史 婚姻爱情 恐怖重口 书画雕刻 建筑行业 奢侈品 自助游 西方艺术 海外游 华语影视 金融 烘焙 舞曲音乐 足球 市场营销 高等院校 中国戏曲 户外运动 舞蹈 教育行业 排球 娱乐八卦 大数据行业 周易风水 养生保健 领导哲学 财经行业 电商行业 日韩游 艺术收藏 其他体育运动 乐器演奏 办公技巧 跑步徒步 医疗医学 舞台剧 歌剧 政府机构 母婴育儿 东南亚游 法制律师 台球 减肥瘦身 韩国影视 国内游 流行音乐 个性服装 房产 瑜伽 马术 乒乓球 曲艺相声 腐女弯男 腕表 魔术 职场智慧 佛学禅道 人力资源行业 羽毛球 IT行业 军事兵器 攀岩 游泳 塑形健身 股票 网球 真人游戏 高尔夫 棋牌 民族音乐 宗教 手工艺品 物流行业 红酒 比特币 语言文字 茶文化 田径 搏击术/格斗术 国外留学 移民 博彩 联想粉 ",
+            "cutText": "随手拍 cosplay 人物摄..."
+        },
+        "myFansInfo": {
+            "taskHasDone": true,
+            "concentrateUpon": "广东",
+            "more": "男"
+        }
+    },
+    "success": true,
+    "userPropertyAbout": {
+        "userWeiboImageUrl": "http://tp3.sinaimg.cn/1898267100/180/40004332098/1",
+        "cntFollowers": "2089",
+        "cntFollowing": "365",
+        "cntPosts": "178",
+        "screenName": "冯豆腐",
+        "userWeiboUrl": "http://weibo.com/u/1898267100",
+        "txtLocation": "中国广东广州",
+        "txtDescription": "Machetevon....",
+        "school": "未认证",
+        "company": " ",
+        "credit": " ",
+        "registedDate": ""
+    }
+}
